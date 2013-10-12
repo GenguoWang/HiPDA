7dbe5964a2cccab0cfc28dadff7571a3627a0ad3
GenguoWang <wanggenguo@outlook.com>
2013-10-12 20:12:32 +0800

    init

---
+++ HiPDA/pages/home/home.js
@@ -0,0 +1,68 @@
+﻿(function () {
+    "use strict";
+    WinJS.UI.Pages.define("/pages/home/home.html", {
+        // 每当用户导航至此页面时都要调用此功能。它
+        // 使用应用程序的数据填充页面元素。
+        ready: function (element, options) {
+            // TODO: 在此处初始化页面。
+            /*
+            var oMyFormData = new FormData();
+            oMyFormData.append("username", "ciceblue");
+            oMyFormData.append("password", "317519");
+            WinJS.xhr({
+                url: "http://www.hi-pda.com/forum/logging.php?action=login&loginsubmit=yes&inajax=1",
+                type: "POST",
+                data: oMyFormData,
+                responseType: "document"
+            }).then(function (result) {
+                console.log("ttsuccess");
+                
+                WinJS.xhr({ "url": "http://www.hi-pda.com/forum/", "responseType": "document" }).then(function (result) {
+                    console.log("success");
+                    var response = result.response;
+                    document.getElementById("output").innerHTML = toStaticHTML(SampleComponent.Example.getEncoding(response.body.innerHTML));
+                    //document.getElementById("output").innerHTML = toStaticHTML(response.body.innerHTML);
+                    WinJS.log && WinJS.log(result);
+                });
+                
+                WinJS.xhr({ url: "http://www.baidu.com/", responseType: "document" }).then(function (result) {
+                    console.log("success");
+                    var response = result.response;
+                    document.getElementById("content").innerHTML = toStaticHTML(response.body.innerHTML);
+                    WinJS.log && WinJS.log(result);
+                });
+            });*/
+            var ex;
+            function basics1() {
+                document.getElementById('output').innerHTML = SampleComponent.Example.getEncoding("这里是个什么地方");
+                SampleComponent.Example.downloadAsStringsAsync("id").then(function (res) {
+                    //document.getElementById('output').innerHTML = res; 
+                    var dom = new DOMParser();
+                    try {
+                        //domd = dom.parseFromString(res, "text/xml");
+                        //dom.parseFromString(
+                        //console.log(domd.body.innerHTML);
+                        //document.implementation.createHTMLDocument();
+                        var doc = document.implementation.createHTMLDocument("example");
+                        doc.documentElement.innerHTML = toStaticHTML(res);
+                        document.getElementById('output').innerHTML = doc.body.innerHTML;
+                    }
+                    catch (e) {
+                        console.log(e.message);
+                    }
+                    console.log("here");
+                });
+
+                
+            }
+
+            function basics2() {
+                ex.sampleProperty += 1;
+                document.getElementById('output').innerHTML += "<br/>" +
+                    ex.sampleProperty;
+            }
+            basics1();
+            //basics2();
+        }
+    });
+})();
---
+++ SampleComponent/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.IO;
+using Windows.Foundation;
+
+namespace SampleComponent
+{
+    public sealed class Example
+    {
+        public static string GetAnswer()
+        {
+            return "The answer is 42.";
+        }
+        public static string GetEncoding(string wgg)
+        {
+            Encoding gb = Encoding.GetEncoding("gb2312");
+            Encoding ut = Encoding.UTF8;
+            byte[] utbyte = ut.GetBytes(wgg);
+            byte[] gbbyte = Encoding.Convert(ut, gb, utbyte);
+            byte[] enn = WebUtility.UrlEncodeToBytes(gbbyte, 0, gbbyte.Length);
+
+            char[] gbChars = new char[ut.GetCharCount(enn, 0, enn.Length)];
+            ut.GetChars(enn, 0, enn.Length, gbChars, 0);
+            string res = new string(gbChars);
+            return res;
+        }
+        /*
+        public static async Task GetHtml()
+        {
+            //WebRequest request = WebRequest.Create("http://www.baidu.com");
+            //WebResponse response = await request.GetResponseAsync();
+            HttpClient client = new HttpClient();
+            Task<byte[]> getContentsTask = client.GetByteArrayAsync("http://www.baidu.com");
+            byte[] urlContents = await getContentsTask;
+            //return urlContents;
+        }*/
+        public static IAsyncOperation<string> DownloadAsStringsAsync(string id)
+        {
+            return Task.Run<string>(async () =>
+            {
+                HttpClient client = new HttpClient();
+                Task<byte[]> getContentsTask = client.GetByteArrayAsync("http://www.baidu.com");
+                byte[] urlContents = await getContentsTask;
+                //Encoding gb = Encoding.GetEncoding("gb2312");
+                Encoding gb = Encoding.UTF8;
+                Encoding ut = Encoding.UTF8;
+                byte[] utbyte = Encoding.Convert(gb, ut, urlContents);
+                char[] utChars = new char[ut.GetCharCount(utbyte, 0, utbyte.Length)];
+                ut.GetChars(utbyte, 0, utbyte.Length, utChars, 0);
+                string res = new string(utChars);
+                return res;
+            }).AsAsyncOperation();
+        }
+        public int SampleProperty { get; set; }
+    }
+}
